c3e6c5f15b851edad3f48be18a086339a6db2ea4
Kirill Duborenko <k.duborenko@gmail.com>
2013-10-14 17:23:39 -0700

    Primitive non-blocking command processing on arduino

--- arduino_fw/arduino_fw.ino
+++ arduino_fw/arduino_fw.ino
@@ -1,17 +1,20 @@
+unsigned long lastCommandTimestamp;
+
 void setup() {
   Serial.begin(9600);
   DDRD = DDRD | B11110000; 
 }
 
 void loop() {
+  if (millis() - lastCommandTimestamp > 200) {
+    PORTD = B0;  
+  }
 }
 
 void serialEvent() {
   while (Serial.available()) {
-    byte val = (byte) Serial.read();
-    PORTD = val;
-    delay(200);
-    PORTD = B0;    
+    PORTD = PORTD | (byte) Serial.read();
+    lastCommandTimestamp = millis();    
   }
 }
 
